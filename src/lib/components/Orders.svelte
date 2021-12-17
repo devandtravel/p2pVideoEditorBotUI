9fb1555534ef17d242d49a72d6fb2eba4e8534a0
devandtravel <devandtravel@gmail.com>
2021-12-17 20:39:58 +0300

    crear code

--- src/lib/components/Orders.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import orders from '/home/romv2/projects_wsl/telegram/p2pVideoEditorBot/files/orders.json';
-</script>
-
-<pre>{JSON.stringify(orders, null, 2)}</pre>
--- src/lib/components/ThemeSwitch.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { theme } from '$lib/stores';
-</script>
-
-<select bind:value={$theme}>
-	<option value="dark">Dark</option>
-	<option value="light">Light</option>
-</select>
